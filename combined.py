77959a3590136fb9a91ed2654a1d9a60d772b12c
jarmstrong22 <143631123+jarmstrong22@users.noreply.github.com>
2024-04-28 19:12:47 -0400

    Update combined.py

--- combined.py
+++ combined.py
@@ -24,7 +24,7 @@ class Graph:
         self.add_node(from_node)
         self.add_node(to_node)
         self.edges[from_node].append((to_node, weight))
-        self.edges[to_node].append((from_node, weight))  # Assuming bidirectional edges
+        #self.edges[to_node].append((from_node, weight))  # Assuming bidirectional edges
     
     def get_neighbors(self, node):
         neighbors = []
@@ -158,28 +158,28 @@ def main():
         [0, -1, 1, -21, 1, 1, -21, 1, 2, 2, 2, 2, 2, -2, 2, 2, 2, 2, 2, -1, 0, 0, 0, 0, 0],
         [0, -1, 1, 0, 1, 1, -21, 1, 1, 1, 2, 2, 2, -2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
         [0, -1, 0, 0, 0, -12, -12, -12, -12, -12, -12, -12, -12, 2, -12, -12, -12, -12, -12, 0, -1, -1, -1, -1, -1],
-        [-1, -1, 0, 0, 3, 2, 2, 2, -212, 2, -122, 2, 2, -2, 2, 2, 2, 2, 2, 0, 4, 4, 4, 7, 7],
+        [-1, -1, 0, 0, 3, 2, 2, 2, -212, 2, -122, 2, 2, -2, 2, 2, 2, 2, 2, 0, 4, 4, 4, -7, 7],
         [-1, 0, 0, 0, 3, 2, 2, 2, -212, 2, -122, 2, 2, -2, 2, 2, 2, 2, 3, 0, 4, 4, 4, 7, 7],
         [-1, 0, 0, 0, 0, 2, 2, 2, -212, 2, -122, 2, 2, -2, 2, 2, 2, 2, 3, 0, 4, 4, 4, 7, 7],
         [-1, 0, 0, 0, 0, 2, 2, 2, -212, 2, -122, 2, 2, -2, 4, 4, 3, 3, 3, 0, 4, 4, 4, 7, 7],
-        [-1, 0, 0, 0, 0, 0, 0, -12, -12, -12, -12, -12, -12, -2, 4, 4, 3, 3, 3, 0, 4, 4, 4, 7, 7],
-        [-1, 0, 0, 5, 5, 5, 2, 2, 2, -6, 2, 2, 2, -2, 4, 4, 5, 4, 4, 0, 8, 7, 7, 7, 7],
-        [-1, 0, 0, 5, 5, 5, 2, 2, 2, -6, 2, 2, 2, -2, 4, 4, 5, 4, 4, 0, 8, 7, 7, 7, 7],
-        [-1, 0, 0, 5, 5, 5, -62, -61, -61, -66, 6, 2, 2, -2, 3, 3, 5, 5, 5, 0, 8, 8, 8, 8, 8],
-        [-1, 0, 0, 5, 5, 5, 6, 6, 6, -6, 2, 2, 2, -2, 3, 3, 5, 5, 5, 0, 8, 8, 8, 8, 8],
-        [-1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8],
-        [0, -1, 0, 0, 5, 5, 7, 7, 7, 13, 13, 13, 13, 0, 10, 10, 10, 5, 5, 5, 5, 5, 5, -1, -1],
-        [0, -1, 0, 0, 5, 5, 5, 7, 7, 13, 13, 13, 13, 0, 10, 10, 10, 5, 5, 5, 5, 5, 5, -1, 0],
-        [0, -1, 0, 0, 5, 5, 5, 12, 13, 13, 13, 13, 12, 0, 10, 10, 10, 5, 5, 5, 5, 5, 5, -1, 0],
-        [0, -1, 0, 0, 5, 5, 5, 12, 12, 12, 12, 12, 12, 0, 10, 10, 10, 5, 5, 5, 5, 5, 5, -1, 0],
-        [0, -1, 0, 0, 5, 5, 12, 12, 12, 12, 12, 12, 12, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, -1, 0],
-        [0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 11, 11, 11, 10, 10, 10, 10, -1, 0],
+        [-1, 0, 0, 0, 0, 0, 0, -12, -12, -12, -12, -12, -12, -2, 4, 4, 3, 3, 3, 0, -4, -44, 4, 7, 7],
+        [-1, 0, 0, 5, -5, 5, 2, 2, 2, -6, 2, 2, 2, -2, 4, 4, 5, 4, -444, 0, 8, -7, 7, 7, 7],
+        [-1, 0, 0, 5, -5, 5, 2, 2, 2, -6, 2, 2, 2, -2, 4, 4, 5, 4, 4, 0, 8, 7, 7, 7, 7],
+        [-1, 0, 0, 5, 5, -55, -62, -61, -61, -66, 6, 2, 2, -2, 3, 3, 5, 5, 5, 0, 8, 8, 8, 8, 8],
+        [-1, 0, 0, 5, 5, -5, -6, 6, 6, -6, 2, 2, 2, -2, 3, -3, -511, 5, 5, 0, 8, 8, 8, 8, 8],
+        [-1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -8, -8, -8, -88, 8],
+        [0, -1, 0, 0, 5, -5, -7, 7, -7, 13, -13, -113, 13, 0, 10, 10, 10, 5, -5, 5, 5, 5, 5, -1, -1],
+        [0, -1, 0, 0, 5, -5, 5, 7, 7, 13, 13, 13, 13, 0, 10, 10, 10, 5, -5, 5, 5, 5, 5, -1, 0],
+        [0, -1, 0, 0, 5, -5, 5, 12, 13, 13, 13, 13, 12, 0, 10, 10, 10, 5, -5, 5, 5, 5, 5, -1, 0],
+        [0, -1, 0, 0, 5, -5, 5, -22222, -112, -112, -112, -2222, 12, 0, 10, 10, 10, 5, -55, 5, 5, 5, 5, -1, 0],
+        [0, -1, 0, 0, -51, -515, 12, -1222, 12, 12, 12, -2212, 12, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, -1, 0],
+        [0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -10, -100, 11, 11, 11, 10, 10, 10, 10, -1, 0],
         [0, -1, 0, 5, 5, 12, 12, 12, 12, 12, 12, 12, 12, 12, 10, 10, 11, 11, 11, 10, 10, 10, 10,-1, 0],
         [0, -1, 0, 0, 0, 12, 12, 12, 12, 12, 12, 12, 12, 12, 10, 10, 11, 11, 11, 10, 10, 10, 10, -1, 0],
-        [0, -1, 3, 3, 3, 12, 12, 12, 12, 12, 12, 12, 12, 12, 10, 10, 11, 11, 11, 10, 10, 10, 10, -1, 0]
+        [0, -1, 3, -3, 3, 12, 12, 12, 12, 12, 12, 12, 12, 12, 10, 10, 11, 11, 11, 10, 10, 10, 10, -1, 0]
     ]
     graph = Graph()
-    valid_values = [-1121, -121, -21, -2121, 0, -12, -122, -212, -6, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1]
+    valid_values = [-1121, -121, -21, -2121, 0, -12, -122, -212, -6, -2, -3, -44, -444, -4,-5, -51, -515, -55, -511, -61, -61, -66, -7, -88, -8, -10, -100, -112, -2212, -1222, -2222, -22222, -13, -113]
 
     for row_index, row in enumerate(matrix):
         for col_index, cell_value in enumerate(row):
@@ -241,33 +241,33 @@ def main():
 
         for i, row in enumerate(matrix):
             for j, ward in enumerate(row):
-                if ward == 8:
+                if ward == 8 or ward == -8 or ward == -88:
                     ward_coordinate_map['ICU'].append((i, j))
                 elif ward == 0: 
                     ward_coordinate_map['Hallway'].append((i,j))
                 elif ward == -1: 
                     ward_coordinate_map['Wall'].append((i,j))
-                elif ward == 4:
+                elif ward in [4,-4,-44,-444]:
                     ward_coordinate_map['ER'].append((i, j))
-                elif ward == 5:
+                elif ward in [-5, -51, -515, -511, -55]:
                     ward_coordinate_map['Oncology'].append((i, j))
-                elif ward == 6:
+                elif ward == 6 or ward == -6 or ward == -61 or ward == -62 or ward == -66:
                     ward_coordinate_map['Burn Ward'].append((i, j))
                 elif ward == 1:
                     ward_coordinate_map['Maternity Ward'].append((i, j))
-                elif ward == 10:
+                elif ward in [10, -10, -100]:
                     ward_coordinate_map['Surgical Ward'].append((i, j))
-                elif ward == 13:
+                elif ward in [-13, 13, -113]:
                     ward_coordinate_map['Hematology'].append((i, j))
-                elif ward == 12:
+                elif ward in [12, -112, -2212, -2222, -22222]:
                     ward_coordinate_map['Pediatric Ward'].append((i, j))
                 elif ward == 11:
                     ward_coordinate_map['Medical Ward'].append((i, j))
-                elif ward == 2:
+                elif ward == 2 or ward == -12 or ward == -212 or ward == -112:
                     ward_coordinate_map['General Ward'].append((i, j))
-                elif ward == 7:
+                elif ward in [7,-7]:
                     ward_coordinate_map['Admissions'].append((i, j))
-                elif ward == 3:
+                elif ward in [3,-3]:
                     ward_coordinate_map['Isolation Ward'].append((i, j))
 
         return ward_coordinate_map
@@ -339,26 +339,75 @@ def main():
     
 
     color_mapping = {
-            0: '#FFFFFF',       # White
-            1: '#ADD8E6',       # Light Blue
-            -1: '#000000',      # Black
-            -2: '#FF0000',      # Red for cell value -2
-            -12: '#FF0000',     # Red up and down wall
-            -122: '#FF0000',     # Red up and down wall
-            -212: '#FF0000',     # Red up and down wall
-            2: '#FF0000',       # Red
-            3: '#B19CD9',       # Light Purple
-            4: '#FFFF00',       # Yellow
-            5: '#90EE90',       # Light Green
-            6: '#6A0DAD',       # Dark Purple
-            7: '#A9A9A9',       # Dark Grey
-            8: '#FFCC80',       # Light Orange
-            10: '#FFC0CB',      # Pink
-            11: '#98FB98',      # Really Light Green
-            12: '#4B5320',      # Army Green
-            13: '#FF8C00'       # Dark Orange
-        }
+        0: '#FFFFFF',       # White
+        1: '#ADD8E6',       # Light Blue
+        -21: '#ADD8E6',      # Vertical light blue walls
+        -2121: '#ADD8E6',    # horizontal light blue walls
+        -1121: '#ADD8E6',
+        -121: '#ADD8E6',
+        -1: '#000000',      # Black
+        -2: '#FF0000',      # Red for cell value -2
+        -12: '#FF0000',     # Red up and down wall
+        -122: '#FF0000',     # Red up and down wall
+        -212: '#FF0000',     # Red up and down wall
+        2: '#FF0000',       # Red
+        3: '#B19CD9',       # Light Purple
+        -3: '#B19CD9',
+        4: '#FFFF00',       # Yellow
+        -44: '#FFFF00', 
+        -444: '#FFFF00', 
+        -4: '#FFFF00', 
+        5: '#90EE90',       # Light Green
+        -5: '#90EE90',
+        -51: '#90EE90',
+        -515: '#90EE90',
+        -511: '#90EE90',
+        -55: '#90EE90',
+        6: '#6A0DAD',       # Dark Purple
+        -6: '#6A0DAD',
+        -61: '#6A0DAD',
+        -62: '#6A0DAD',
+        -66: '#6A0DAD',
+        7: '#A9A9A9',       # Dark Grey
+        -7: '#A9A9A9',  
+        8: '#FFCC80',       # Light Orange
+        -88: '#FFCC80',  
+        -8: '#FFCC80',  
+        10: '#FFC0CB',      # Pink
+        -10: '#FFC0CB', 
+        -100: '#FFC0CB', 
+        11: '#98FB98',      # Really Light Green
+        -112: '#4B5320',      # Army Green
+        -2212: '#4B5320', 
+        -1222: '#4B5320', 
+        -2222: '#4B5320',   
+        -22222: '#4B5320',   
+        12: '#4B5320',   
+        13: '#FF8C00',       # Dark Orange
+        -13: '#FF8C00',   
+        -113: '#FF8C00'
+    }
     
+    def draw_wall_right(x,y):
+        line_start_right = ((y + 1) * cell_size, x * cell_size)
+        line_end_right = ((y + 1) * cell_size, (x + 1) * cell_size)
+        canvas.create_line(line_start_right, line_end_right, fill='#000000',width=2)
+
+    def draw_wall_left(x,y):
+        line_start_left = (y * cell_size, x * cell_size)
+        line_end_left = (y * cell_size, (x + 1) * cell_size)
+        canvas.create_line(line_start_left, line_end_left, fill='#000000',width=2)
+
+    def draw_wall_top(x, y):
+        line_start_top = (y * cell_size, x * cell_size)
+        line_end_top = ((y + 1) * cell_size, x * cell_size)
+        canvas.create_line(line_start_top, line_end_top, fill='#000000',width=2)
+    
+    def draw_wall_bottom(x, y):
+        line_start_bottom = (y * cell_size, (x + 1) *cell_size)
+        line_end_bottom = ((y + 1) * cell_size, (x + 1) * cell_size)
+        canvas.create_line(line_start_bottom, line_end_bottom, fill='#000000',width=2)
+
     def draw_maze():
         for x in range(rows):
             for y in range(cols):
@@ -378,37 +427,146 @@ def main():
                     fill=color, outline='',  # Set outline to empty string to remove the border
                 )
 
-                if maze[x][y] in [-1, -2, -12, -122]:  # Check if the cell is a wall (-1 or -2)
-                    if maze[x][y] == -2: 
-                        # Draw black lines along the farthest right and left sides of the cell wall
-                        line_start_left = (y * cell_size, x * cell_size)
-                        line_end_left = (y * cell_size, (x + 1) * cell_size)
-                        line_start_right = ((y + 1) * cell_size, x * cell_size)
-                        line_end_right = ((y + 1) * cell_size, (x + 1) * cell_size)
-                        canvas.create_line(line_start_left, line_end_left, fill='#000000', width=2)
-                        canvas.create_line(line_start_right, line_end_right, fill='#000000', width=2)
+                if maze[x][y] == -2:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -21:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+                    draw_wall_bottom(x,y)
+                
+                if maze[x][y] == -21:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -6:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
 
-                    if maze[x][y] == -12:  # Check if the cell value is -12
-                        # Draw black lines along the top and bottom sides of the cell wall
-                        line_start_top = (y * cell_size, x * cell_size)
-                        line_end_top = ((y + 1) * cell_size, x * cell_size)
-                        line_start_bottom = (y * cell_size, (x + 1) * cell_size)
-                        line_end_bottom = ((y + 1) * cell_size, (x + 1) * cell_size)
-                        canvas.create_line(line_start_top, line_end_top, fill='#000000', width=2)
-                        canvas.create_line(line_start_bottom, line_end_bottom, fill='#000000', width=2)
+                if maze[x][y] == -121:
+                    draw_wall_bottom(x,y)
+                    draw_wall_top(x,y)
 
-                    if maze[x][y] == -122: 
-                        # Draw black lines along right cell wall
-                        line_start_right = ((y + 1) * cell_size, x * cell_size)
-                        line_end_right = ((y + 1) * cell_size, (x + 1) * cell_size)
-                        canvas.create_line(line_start_right, line_end_right, fill='#000000', width=2)
+                if maze[x][y] == -12:
+                    draw_wall_bottom(x,y)
+                    draw_wall_top(x,y)
+                
+                if maze[x][y] == -61:
+                    draw_wall_bottom(x,y)
+                    draw_wall_top(x,y)
+                    
+                if maze[x][y] == -1121:
+                    draw_wall_top(x,y)
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -62:
+                    draw_wall_top(x,y)
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -2121:
+                    draw_wall_top(x,y)
+                    draw_wall_right(x,y)
 
-                    if maze[x][y] == -212: 
-                        # Draw black lines along the farthest right and left sides of the cell wall
-                        line_start_left = (y * cell_size, x * cell_size)
-                        line_end_left = (y * cell_size, (x + 1) * cell_size)
-                        canvas.create_line(line_start_left, line_end_left, fill='#000000', width=2)
+                if maze[x][y] == -212:
+                    draw_wall_left(x,y)
 
+                if maze[x][y] == -66:
+                    draw_wall_right(x,y)
+                
+                if maze[x][y] == -122:
+                    draw_wall_right(x,y)
+
+                if maze[x][y] == -55:
+                    draw_wall_top(x,y)
+                    draw_wall_right(x,y)
+                
+                if maze[x][y] == -5:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+
+                if maze[x][y] == -55:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -51:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+
+                if maze[x][y] == -511:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+                    draw_wall_top(x,y)
+                
+                if maze[x][y] == -3:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+                    draw_wall_top(x,y)
+                
+                if maze[x][y] == -13:
+                    draw_wall_left(x,y)
+                    draw_wall_bottom(x,y)
+                
+                if maze[x][y] == -113:
+                    draw_wall_right(x,y)
+                    draw_wall_bottom(x,y)
+                
+                if maze[x][y] == -1222:
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -112:
+                    draw_wall_top(x,y)
+                
+                if maze[x][y] == -2212:
+                    draw_wall_right(x,y)
+
+                if maze[x][y] == -515:
+                    draw_wall_right(x,y)
+                    draw_wall_bottom(x,y)
+
+                if maze[x][y] == -7:
+                    draw_wall_right(x,y)
+                    draw_wall_left(x,y)
+                    draw_wall_bottom(x,y)
+                
+                if maze[x][y] == -4:
+                    draw_wall_top(x,y)
+                    draw_wall_bottom(x,y)
+                
+                if maze[x][y] == -44:
+                    draw_wall_right(x,y)
+                    draw_wall_top(x,y)
+                
+                if maze[x][y] == -444:
+                    draw_wall_bottom(x,y)
+                    draw_wall_top(x,y)
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -2222:
+                    draw_wall_top(x,y)
+                    draw_wall_right(x,y)
+                
+                if maze[x][y] == -22222:
+                    draw_wall_top(x,y)
+                    draw_wall_left(x,y)
+                
+                if maze[x][y] == -8:
+                    draw_wall_top(x,y)
+                    draw_wall_bottom(x,y)
+                
+                if maze[x][y] == -88:
+                    draw_wall_top(x,y)
+                    draw_wall_bottom(x,y)
+                    draw_wall_right(x,y)
+                
+                if maze[x][y] == -10:
+                    draw_wall_top(x,y)
+                    draw_wall_bottom(x,y)
+
+                if maze[x][y] == -100:
+                    draw_wall_bottom(x,y)
+                    draw_wall_right(x,y)
+                    draw_wall_top(x,y)
     draw_maze()
 
     def draw_path():
